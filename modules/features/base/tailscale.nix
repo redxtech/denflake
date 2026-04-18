4eb3ae705a6d01603a9692b0ab9fd23fbd47ab97
Gabe Dunn <gabe@gabedunn.dev>
2026-04-17 23:58:42 -0700

    feat: add tailscale auto-login

--- modules/features/base/tailscale.nix
+++ modules/features/base/tailscale.nix
@@ -1,7 +1,18 @@
 { inputs, self, ... }:
 
 {
-  den.aspects.tailscale = {
-    nixos.services.tailscale.enable = true;
-  };
+  den.aspects.tailscale =
+    { host, ... }:
+    {
+      nixos =
+        { config, ... }:
+        {
+          services.tailscale = {
+            enable = true;
+            authKeyFile = config.sops.secrets.tailscale-init-authkey.path;
+          };
+
+          sops.secrets.tailscale-init-authkey = { };
+        };
+    };
 }
